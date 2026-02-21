a353ba4effbc18e172c707509c6eaea3ba0605d0
Rohit kukreja <48058107+rohitkukreja705@users.noreply.github.com>
2026-02-21 12:49:04 +0530

    Update attendance.js

--- js/attendance.js
+++ js/attendance.js
@@ -111,7 +111,7 @@ async function getPublicIP(){
 function getIST(){ const d=new Date(); return parseInt(new Intl.DateTimeFormat("en-IN",{timeZone:"Asia/Kolkata",hour:"2-digit",hour12:false}).format(d)); }
 
 async function startAttendance(){
-  if(getIST()<21){ alert("Attendance allowed 9PM–10PM"); return; }
+  if(getIST()<10){ alert("Attendance allowed 9PM–10PM"); return; }
 
   showLoader(); setProgress(15);
 
