295d42c96df7d3bd34770b7d3ac7d57d6261428d
Rohit kukreja <48058107+rohitkukreja705@users.noreply.github.com>
2026-02-21 12:12:11 +0530

    Update attendance.js

--- js/attendance.js
+++ js/attendance.js
@@ -1,7 +1,6 @@
 const video = document.getElementById("video");
 const statusText = document.getElementById("status");
 const GAS_URL = "https://script.google.com/macros/s/AKfycbxGLHhVupOddlZrDHvqBq4n084qT1uFbHV3VPioTmQyImLt66w5QRc3lj09nwp-eh0k/exec";
-const ipEl = document.getElementById("liveIP");
 const HOSTEL_LAT = 23.250761280;
 const HOSTEL_LNG = 77.499552907;
 const ALLOWED_RADIUS = 50; // meters
